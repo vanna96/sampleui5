0e6ca414c13cee4028f87c0f57a7636d30cf86d2
vanna96 <sovannapoung@gmail.com>
2025-01-31 20:07:19 +0700

    update item

--- webapp/controller/Item.controller.js
+++ webapp/controller/Item.controller.js
@@ -15,21 +15,22 @@ sap.ui.define([
 
         onInit: function () {
             document.title = "Item Master Data";
-            this.oModel = new sap.ui.model.json.JSONModel({});  
-			this.getView().setModel(this.oModel, "model");
+            this.oModel = new sap.ui.model.json.JSONModel({});
+            this.getView().setModel(this.oModel, "model");
 
             this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             this.oRouter.getRoute("item").attachPatternMatched(this.onItemListing, this);
+            this.oRouter.getRoute("item_create").attachPatternMatched(this.onItemCreate, this);
         },
 
-        formatDate: function(oDate){ 
+        formatDate: function (oDate) {
             if (oDate) {
                 return Helper.dateFormat({ format: 'dd MMM, yyyy', value: oDate })
             }
             return "";
         },
 
-        onItemListing: async function () { 
+        onItemListing: async function () {
             this.oModel.setData({
                 pagination: {
                     currentPage: 1,
@@ -39,20 +40,20 @@ sap.ui.define([
                     hasPrevious: false,
                     hasNext: false
                 },
-                isLoading: true,  
-                search: {}  
-            }); 
+                isLoading: true,
+                search: {}
+            });
 
             await this.loadData(1);
             this.oModel.setProperty("/isLoading", false);
         },
 
-        loadData: async function (pageNumber) { 
+        loadData: async function (pageNumber) {
             this.oModel.setProperty("/isLoading", true);
             const pageSize = this.oModel.getProperty("/pagination/pageSize");
             const search = this.oModel.getProperty("/search");
-            const skip = (pageNumber - 1) * pageSize; 
-            const filters = []; 
+            const skip = (pageNumber - 1) * pageSize;
+            const filters = [];
 
             const oParams = {
                 $top: pageSize,
@@ -60,7 +61,7 @@ sap.ui.define([
                 $count: true
             };
 
-            if (search){
+            if (search) {
                 // if (search.Search) filters.push(search.Search);
                 // if (search.CustomerName) filters.push(search.CustomerName);
                 // if (search.DocumentNo) filters.push(search.DocumentNo);
@@ -71,49 +72,118 @@ sap.ui.define([
             }
 
             if (filters.length > 0) oParams.$filter = filters.join(' and ');
-            
-            try { 
-                const data = await ItemRepository.get(oParams);  
-                const totalItems = data["odata.count"]; 
-                const totalPages = Pagination.getTotalPages(totalItems, pageSize); 
+
+            try {
+                const data = await ItemRepository.get(oParams);
+                const totalItems = data["odata.count"];
+                const totalPages = Pagination.getTotalPages(totalItems, pageSize);
                 this.oModel.setProperty("/data", data.value);
                 this.oModel.setProperty("/pagination/totalItems", totalItems);
                 this.oModel.setProperty("/pagination/totalPages", totalPages);
                 const paginationInfo = Pagination.getPaginationInfo(totalItems, pageSize, pageNumber);
-                this.oModel.setProperty("/pagination", paginationInfo); 
+                this.oModel.setProperty("/pagination", paginationInfo);
 
-            } catch (error){
+            } catch (error) {
                 console.error("Error loading data:", error);
-            } 
+            }
             this.oModel.setProperty("/isLoading", false);
         },
 
-        onNextPage: function (){
+        onNextPage: function () {
             let currentPage = this.oModel.getProperty("/pagination/currentPage");
             const totalPages = this.oModel.getProperty("/pagination/totalPages");
 
-            if (currentPage < totalPages){
+            if (currentPage < totalPages) {
                 currentPage++;
                 this.loadData(currentPage);
             }
         },
 
-        onPreviousPage: function (){
+        onPreviousPage: function () {
             let currentPage = this.oModel.getProperty("/pagination/currentPage");
 
-            if (currentPage > 1){
+            if (currentPage > 1) {
                 currentPage--;
                 this.loadData(currentPage);
             }
         },
 
-        onFirstPage: function (){
+        onFirstPage: function () {
             this.loadData(1);
         },
 
-        onLastPage: function (){
+        onLastPage: function () {
             const totalPages = this.oModel.getProperty("/pagination/totalPages");
             this.loadData(totalPages);
+        },
+
+        // form
+        onCreate: function () {
+            this.oRouter.navTo("item_create");
+        },
+
+        _resetForm: function () {
+            this.oModel.setData({
+                titleForm: document.title,
+                buttonSubmit: "Save",
+                status: true,
+                statusList: [
+                    {
+                        "name": "Active",
+                        "value": true,
+                    },
+                    {
+                        "name": "Inactive",
+                        "value": false,
+                    }
+                ],
+                currencyList: [
+                    {
+                        "name": "Dollar($)",
+                        "value": 1,
+                    },
+                    {
+                        "name": "Reil(៛)",
+                        "value": 2,
+                    }
+                ],
+                uomGroupList: [
+                    {
+                        "name": "Manual",
+                        "value": 1,
+                    },
+                    {
+                        "name": "Littre",
+                        "value": 2,
+                    }
+                ],
+                priceLists: [
+                    {
+                        "name": "Base Price",
+                        "value": 1,
+                    },
+                    {
+                        "name": "Retail Sale",
+                        "value": 2,
+                    }
+                ],
+                itemType: [
+                    {
+                        "name": "UoM Setup",
+                    },
+                    {
+                        "name": "E Commerce / Variation Setup",
+                    },
+                    {
+                        "name": "Food / add on Setup",
+                    }
+                ]
+            });
+        },
+
+        onItemCreate: async function () {
+            document.title = "Create Item Master Data";
+            this._resetForm()
         },
     });
 });
